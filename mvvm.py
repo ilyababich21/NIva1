9ed39fc90268efa6c0e9b8491e5c403a9ac3e5be
ilya_babich <arkhamnight0123@gmail.com>
2023-02-20 17:46:08 +0300

    hello

--- mvvm.py
+++ mvvm.py
@@ -31,7 +31,7 @@ class ExampleApp(QtWidgets.QMainWindow):
         self.modbusForm = ModbusForm()
         self.ping = Ping()
         uic.loadUi(UI_autoriation, self)
-        conn = psycopg2.connect(dbname="postgres", user="postgres", password="root", host="127.0.0.1")
+        conn = psycopg2.connect(dbname="postgres", user="postgres", password="1111", host="127.0.0.1")
         cursor = conn.cursor()
 
         conn.autocommit = True
@@ -44,7 +44,7 @@ class ExampleApp(QtWidgets.QMainWindow):
         cursor.close()
         conn.close()
 
-        con = psycopg2.connect(dbname='niva1', user='postgres', password='root', host='127.0.0.1')
+        con = psycopg2.connect(dbname='niva1', user='postgres', password='1111', host='127.0.0.1')
 
         cursor = con.cursor()
         con.autocommit = True
@@ -160,7 +160,7 @@ class ExampleApp(QtWidgets.QMainWindow):
             people)
 
         uic.loadUi(UI_autoriation, self)
-        conn = psycopg2.connect(dbname="postgres", user="postgres", password="root", host="127.0.0.1")
+        conn = psycopg2.connect(dbname="postgres", user="postgres", password="1111", host="127.0.0.1")
         cursor = conn.cursor()
 
         conn.autocommit = True
@@ -172,7 +172,7 @@ class ExampleApp(QtWidgets.QMainWindow):
         cursor.close()
         conn.close()
 
-        con = psycopg2.connect(dbname='niva1', user='postgres', password='root', host='127.0.0.1')
+        con = psycopg2.connect(dbname='niva1', user='postgres', password='1111', host='127.0.0.1')
 
         cursor = con.cursor()
         con.autocommit = True

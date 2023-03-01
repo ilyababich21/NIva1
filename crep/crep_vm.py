2d2101613e85d713c29c923abb7b3b287ed0c661
ilya_babich <arkhamnight0123@gmail.com>
2023-03-01 18:56:40 +0300

    autoadd buttons + ui_crep

---
+++ crep/crep_vm.py
@@ -0,0 +1,14 @@
+from PyQt6 import uic, QtWidgets
+
+UI_crep = "view/ifc_crep.ui"
+
+
+
+
+class CrepViewModel(QtWidgets.QMainWindow):
+    def __init__(self,num):
+        super().__init__()
+
+        uic.loadUi(UI_crep, self)
+        self.num_crep.setText(str(num))
+
--- ifc/ifc_vm.py
+++ ifc/ifc_vm.py
@@ -1,11 +1,12 @@
 from PyQt6 import uic, QtWidgets
-
+from crep.crep_vm import CrepViewModel
 UI_ifc = "view/ifc_model_view.ui"
 
 
-class ButtonForSection(QtWidgets.QMainWindow):
+class ButtonForSection(QtWidgets.QPushButton):
     def __init__(self, number):
         super().__init__()
+        self.id = number
 
 
 
@@ -14,3 +15,21 @@ class IfcViewModel(QtWidgets.QMainWindow):
         super().__init__()
 
         uic.loadUi(UI_ifc, self)
+
+        self.make_buttons(self.layout_600)
+        self.make_buttons(self.layout_200)
+
+
+
+    def make_buttons(self,layout):
+        num = 10
+        for elem in range(num):
+            btn = ButtonForSection(elem + 1)  # !!!
+            btn.clicked.connect(lambda ch, b=btn: self.on_clicked(b))
+            layout.addWidget(btn)
+
+    def on_clicked(self,btn):
+        self.crep = CrepViewModel(btn.id)
+        self.crep.show()
+
+

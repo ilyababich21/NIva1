6c11e3a59a2b7efe60e89524dc94b056ffbfd1e8
Pavel_http <79783489+pashatap@users.noreply.github.com>
2023-03-01 17:35:30 +0300

    create class button for section

--- ifc/ifc_vm.py
+++ ifc/ifc_vm.py
@@ -3,6 +3,12 @@ from PyQt6 import uic, QtWidgets
 UI_ifc = "view/ifc_model_view.ui"
 
 
+class ButtonForSection(QtWidgets.QMainWindow):
+    def __init__(self, number):
+        super().__init__()
+
+
+
 class IfcViewModel(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()

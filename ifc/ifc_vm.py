5d80e0165f1a75626dc2419406afb2a4736141f4
Pavel_http <79783489+pashatap@users.noreply.github.com>
2023-03-06 15:26:02 +0300

    create view datasensors ui and work,and review ifc model

--- ifc/ifc_vm.py
+++ ifc/ifc_vm.py
@@ -1,39 +1,47 @@
 from PyQt6 import uic, QtWidgets
+
 from crep.crep_vm import CrepViewModel
+from dataSensors.data_sensors_vm import DataSensors
 
 UI_ifc = "view/ifc version1.ui"
 
 
 class ButtonForSection(QtWidgets.QPushButton):
-    def __init__(self, number):
+    def __init__(self, number, size):
         super().__init__()
         self.id = number
+        self.setFixedSize(*size)
 
 
 class IfcViewModel(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-
+        self.data_sensors = DataSensors()
         uic.loadUi(UI_ifc, self)
-
         self.make_buttons(self.layout_600)
-        self.make_buttons(self.layout_200)
+        self.make_buttons(self.layout_250)
         self.make_buttons(self.layout_1000)
         self.make_buttons(self.layout_10000)
-        self.make_buttons(self.layout_250)
+        self.make_buttons(self.layout_200)
         self.make_buttons(self.layout_6)
+        self.data_sensors_pushButton.clicked.connect(self.show_data_sensors)
+
 
     def make_buttons(self, layout):
-
-        num = self.section_max_lineEdit.text()
-        if num == "":
-            num = 0
-        num = int(num)
+        self.size_of_button = (20, 150)
+        # num = self.section_max_lineEdit.text()
+        # if num == "":
+        #     num = 0
+        # num = int(num)
+        num = 100
         for elem in range(num):
-            btn = ButtonForSection(elem + 1)  # !!!
+            btn = ButtonForSection(elem + 1, self.size_of_button)  # !!!
             btn.clicked.connect(lambda ch, b=btn: self.on_clicked(b))
             layout.addWidget(btn)
 
     def on_clicked(self, btn):
         self.crep = CrepViewModel(btn.id)
         self.crep.show()
+
+    def show_data_sensors(self):
+        self.data_sensors.show()

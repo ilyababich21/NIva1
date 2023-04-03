8801a16fb0ed2320591f1602ddefff2b940bb40b
ilyababich21 <arkhamnight0123@gmail.com>
2023-04-03 10:14:52 +0300

    build buttons for all layouts, rebuild async_func

--- ifcApp/ifc/ifc_vm.py
+++ ifcApp/ifc/ifc_vm.py
@@ -10,7 +10,7 @@ from ifcApp.dataSensors.data_sensors_vm import DataSensorsMainWindow
 from ifcApp.dataSensors.settings_data_sensors_vm import SettingsSensors
 
 UI_ifc = "view/ifc/ifc version1.ui"
-# fvdv
+
 
 class WorkerSignals(QObject):
 

77664614553bc81ae65011fa1e9bf8ee7a585af3
Pavel_http <79783489+pashatap@users.noreply.github.com>
2023-06-21 11:13:09 +0300

    working add and delete users

--- ifcApp/ifc/users/users_in_ifc_vm.py
+++ ifcApp/ifc/users/users_in_ifc_vm.py
@@ -1,5 +1,5 @@
 from PyQt6 import uic, QtGui
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QMessageBox
 
 from authorization.authorization_model import Users
 from ifcApp.ifc.users.groupbox_for_users import GroupBoxForUser
@@ -11,24 +11,30 @@ UI_user = "view/ifc/user.ui"
 class UserInIfc(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.list_groupbox_radiobutton = []
-        self.list_users_login = []
 
+        self.users = None
+        self.groupbox_in_users = None
+        self.list_users_login = None
+        self.list_groupbox_for_users = None
         self.load_UI()
 
     def load_UI(self):
+        self.list_groupbox_for_users = []
+        self.list_users_login = []
         uic.loadUi(UI_user, self)
         self.users = session.query(Users).all()
         for user in self.users:
             self.groupbox_in_users = GroupBoxForUser()
-            if not len(self.users) == len(self.list_groupbox_radiobutton):
-                self.list_groupbox_radiobutton.append(self.groupbox_in_users.radioButton)
+            if not len(self.users) == len(self.list_groupbox_for_users):
+                self.list_groupbox_for_users.append(self.groupbox_in_users)
                 self.list_users_login.append(user.login)
             self.groupbox_in_users.username_label.setText(user.login)
             if user.role_id == 3:
                 self.groupbox_in_users.pixmap.setPixmap(QtGui.QPixmap("image/user/user_admin.png"))
+                self.groupbox_in_users.admin_pushButton.setStyleSheet(" background-color: #00ff00;")
             elif user.role_id == 4:
                 self.groupbox_in_users.pixmap.setPixmap(QtGui.QPixmap("image/user/user_control.png"))
+                self.groupbox_in_users.pitman_pushButton.setStyleSheet(" background-color: #00ff00;")
             self.layout_user_groupbox.addWidget(self.groupbox_in_users)
         self.add_user_pushButton.clicked.connect(self.show_add_user)
         self.delete_user_pushButton.clicked.connect(self.delete_user)
@@ -38,20 +44,29 @@ class UserInIfc(QMainWindow):
         self.add.clicked.connect(self.add_to_database_on_clicked)
 
     def add_to_database_on_clicked(self):
+        if self.law.currentText() == "Администратор":
+            value = 3
+        else:
+            value = 4
         session.add_all([Users(login=f"{self.username.text()}", password=f"{self.password.text()}", manufacture_id=1,
-                               role_id=f"{int(self.law.text())}")])
+                               role_id=f"{value}")])
         session.commit()
 
         self.load_UI()
-        print(self.list_groupbox_radiobutton)
 
     def delete_user(self):
-        print(f"leng{self.list_groupbox_radiobutton}")
-        for action in range(len(self.list_groupbox_radiobutton)):
-            if self.list_groupbox_radiobutton[action].isChecked():
-                delete_query = session.query(Users).filter(Users.login == self.list_users_login[action]).first()
-                session.delete(delete_query)
-                session.commit()
-                self.list_groupbox_radiobutton.clear()
-                self.list_users_login.clear()
-                self.load_UI()
+        try:
+            for action in range(len(self.list_groupbox_for_users)):
+                if self.list_groupbox_for_users[action].radioButton.isChecked():
+                    delete_query = session.query(Users).filter(Users.login == self.list_users_login[action]).first()
+                    session.delete(delete_query)
+                    session.commit()
+                    self.load_UI()
+        except:
+            print("you delete two users")
+
+    # def change_user(self):
+    #     uic.loadUi("view/ifc/add user.ui", self)
+    #     self.label.setText("Изменение пользователя")
+    #     for elem in range(len(self.list_groupbox_for_users)):
+    #         self.username.setText(f"{self.list_users_login[elem]}")
